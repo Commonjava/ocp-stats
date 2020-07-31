06c9e8fce55c9426093d0d3d1247a18e453ca714
John Casey <jdcasey@commonjava.org>
2020-07-31 16:51:46 -0500

    Fix invalid syntax

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ print('OpenShift client version: {}'.format(oc.get_client_version()))
 print('OpenShift server version: {}'.format(oc.get_server_version()))
 
 while True:
-    nodes = oc.selector('nodes').objects():
+    nodes = oc.selector('nodes').objects()
     print(nodes)
 
     print('Sleeping 30 seconds')
