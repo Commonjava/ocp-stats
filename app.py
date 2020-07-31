8c4b3309fd8aa20b56b08989728f1b51ad7fdd45
John Casey <jdcasey@commonjava.org>
2020-07-31 16:49:50 -0500

    try to read node list to understand the structure.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import openshift as oc
 import sys
+from time import sleep
 
 print('Python version: {}'.format(sys.version))
 print('OpenShift client version: {}'.format(oc.get_client_version()))
 print('OpenShift server version: {}'.format(oc.get_server_version()))
 
 while True:
-	#nop
-	
+    nodes = oc.selector('nodes').objects():
+    print(nodes)
+
+    print('Sleeping 30 seconds')
+    sleep(30)
